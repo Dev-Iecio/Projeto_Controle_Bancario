a3d0ca12bb7e5a3fc7bcb6971f4dda2209a7049d
Gabriel Dalecio <dalecio321@gmail.com>
2024-10-23 15:40:25 -0300

    Feito a estrutura da lista simplesmente emcadeada.

---
+++ funçoes.h
@@ -0,0 +1,48 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+typedef struct 
+{
+int codigo_conta;
+char banco[50];
+char agencia [10];
+char numero_conta[50];
+char tipo_conta [10];
+float v1_saldo;
+float v1_limite;
+int status;
+}reg_contas;
+
+typedef struct tipoitem *tipoApontador_conta;
+
+typedef struct tipoitem_conta
+{
+    reg_contas conteudo;
+    tipoApontador_conta proximo;
+
+}tipoitem_conta;
+
+typedef struct
+{
+tipoApontador_conta primeiro;
+tipoApontador_conta ultimo;
+}tipolista_conta;
+
+typedef struct {
+    int sequencial;
+    int codigo_conta;
+    char dt_movimento[10];
+    char tp_movimento[10];
+    float v1_movimento;
+    float v1_saldo;
+}reg_movimentos;
+
+void gotoxy(int x,int y);
+
+void tela();
+
+void menu_consulta_cotas(tipolista_conta *l);
+
+void menu_contas_bancarias(tipolista_conta *l);
+
+#endif
